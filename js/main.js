983a0daa20323b6d9d7cf8fd127e04268b129568
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-10-24 22:51:38 +0200

    add: for statement, terminato senza bonus

--- js/main.js
+++ js/main.js
@@ -7,3 +7,15 @@ Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro program
 BONUS 1: Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
 BONUS 2: Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna. */
+for (var i = 1; i < 101; i++) {
+    if (i % 15 == 0){
+        console.log("FizzBuzz");
+    } 
+    else if(i % 3 == 0){
+         console.log("Fizz");
+    } 
+    else if (i % 5 == 0){
+        console.log("Buzz");
+    } 
+    console.log(i);
+}
