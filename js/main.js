bab04ef837cbacb91b49efbd447a12dc413da29b
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-10-25 00:21:44 +0200

    printate le informazioni a schermo

--- js/main.js
+++ js/main.js
@@ -7,15 +7,22 @@ Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro program
 BONUS 1: Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
 BONUS 2: Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna. */
-for (var i = 1; i < 101; i++) {
+const board= document.querySelector(".board")
+
+for (let i = 1; i < 101; i++) {
+    const box = document.createElement('div');
     if (i % 15 == 0){
-        console.log("FizzBuzz");
+        box.innerHTML = "FizzBuzz";
     } 
     else if(i % 3 == 0){
-         console.log("Fizz");
+        box.innerHTML = "Fizz";
     } 
     else if (i % 5 == 0){
-        console.log("Buzz");
+        box.innerHTML = "Buzz";
     } 
-    console.log(i);
+    else{
+        box.innerHTML = i;
+    }
+    box.classList.add('box');
+    board.append(box);
 }
